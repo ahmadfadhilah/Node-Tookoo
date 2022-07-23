64b598ef066271842707ca1e4f50e7d58fea1c19
ahmadfadhilah <stevenasep000@gmail.com>
2022-07-23 10:45:54 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send("Hii");
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.json());
+app.get("/Id/:id/Name/:name", (req, res) => {
+    res.send({
+        message: "hello",
+        data: req.params.id,
+        name: req.params.name
+    });
+});
+app.post("/Id/:id/Name/:name", (req, res) => {
+    res.send({
+        data: req.body,
+        params: {
+            id: req.params.id,
+            name: req.params.name
+        }
+    });
 });
 app.listen(3000, () => {
     console.log("server running successfully on port 3000!");
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import express from "express";
+import {Request, Response} from 'express';
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hii");
+app.get("/Id/:id/Name/:name", (req: Request, res: Response) => {
+  res.send({
+    message: "hello",
+    data: req.params.id,
+    name: req.params.name
+  });
+});
+
+app.post("/Id/:id/Name/:name", (req: Request, res: Response) => {
+  res.send({
+    data: req.body,
+    params: {
+      id: req.params.id,
+      name: req.params.name
+    }
+  });
 });
 
 app.listen(3000, () => {
